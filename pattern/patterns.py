ef817d472874c3f13d2e645051c671a8deee3955
Rizwan Ahmed <rizz@INPC0070.local>
2025-06-08 11:05:47 +0530

    feat: added patters questions

---
+++ pattern/patterns.py
@@ -0,0 +1,117 @@
+def pattern1():
+    """
+    * * * * * 
+    * * * * * 
+    * * * * * 
+    * * * * * 
+    * * * * *
+    """
+    for _ in range(5):
+        for _ in range(5):
+            print("*", end=" ")
+        print("")
+
+def pattern2():
+    """
+    * 
+    * * 
+    * * * 
+    * * * * 
+    * * * * * 
+    """
+    for i in range(6):
+        for _ in range(i):
+            print("*", end=" ")
+        print("")
+
+def pattern3():
+    """
+    1 
+    1 2 
+    1 2 3 
+    1 2 3 4 
+    1 2 3 4 5 
+    """
+    for i in range(1,6):
+        for j in range(1,i+1):
+            print(j, end=" ")
+        print("")
+
+def pattern4():
+    """
+    1 
+    2 2 
+    3 3 3 
+    4 4 4 4 
+    5 5 5 5 5 
+    """
+    for i in range(1,6):
+        for _ in range(1,i+1):
+            print(i, end=" ")
+        print("")
+
+def pattern5():
+    """
+    * * * * * 
+    * * * * 
+    * * * 
+    * * 
+    * 
+    """
+    for i in range(1,6):
+        for _ in range(5,i-1,-1):
+            print("*", end=" ")
+        print("")
+        
+def pattern6():
+    """
+    1 2 3 4 5 
+    1 2 3 4 
+    1 2 3 
+    1 2 
+    1 
+    """
+    for i in range(6):
+        for j in range(1,6-i):
+            print(j, end=" ")
+        print("")
+
+def pattern7():
+    """
+            * 
+          * * * 
+        * * * * * 
+      * * * * * * * 
+    * * * * * * * * * 
+    """
+    for i in range(1,6):
+        for _ in range(5-i):
+            print(" ", end=" ")
+        for _ in range((i*2)-1):
+            print("*", end=" ")
+        print("")
+
+def pattern8():
+    """
+    * * * * * * * * * 
+      * * * * * * * 
+        * * * * * 
+          * * * 
+            * 
+    """
+    for i in range(5,0,-1):
+        for _ in range(5-i):
+            print(" ", end=" ")
+        for _ in range((i*2)-1):
+            print("*", end=" ")
+        print("")
+        
+    
+pattern1()
+pattern2()
+pattern3()
+pattern4()
+pattern5()
+pattern6()
+pattern7()
+pattern8()
